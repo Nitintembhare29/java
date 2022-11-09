ba12ddcb4b6fcea5ebe3c84f663e21f75ed9feb9
Ravi <asdf>
2022-11-09 18:47:20 +0530

    k

---
+++ tempCodeRunnerFile.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Main     
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+Scanner sc = new Scanner(System.in);
+System.out.println("Enter two number");
+int n = sc.nextInt();
+System.out.println(n);
+    }
+}
